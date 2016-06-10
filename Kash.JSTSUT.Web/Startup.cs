0338cbd3a1d735132e0614940c20c994b7440d87
Kash <castillo.sergio@gmail.com>
2016-06-10 14:58:45 +0200

    Inicio del proyecto MVC 5, tal cual

---
+++ Kash.JSTSUT.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kash.JSTSUT.Web.Startup))]
+namespace Kash.JSTSUT.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
