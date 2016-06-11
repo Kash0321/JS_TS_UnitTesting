20dae5721cf99f723ae04439df683104dc136edb
Kash <castillo.sergio@gmail.com>
2016-06-12 01:47:33 +0200

    Sistema de tareas para Gulp (desarrollo) operativo

--- src/Kash.JSTSUT.Web/gulpfile.js
+++ src/Kash.JSTSUT.Web/gulpfile.js
@@ -1,10 +1,11 @@
-﻿/*
+﻿/// <binding BeforeBuild='_dev:build' Clean='cmn:clear' />
+/*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp'),
-  rimraf = require('gulp-rimraf'),
+  del = require('del'),
   concat = require('gulp-concat'),
   rename = require('gulp-rename'),
   sourcemaps = require('gulp-sourcemaps'),
@@ -29,21 +30,45 @@ var roots = {
 
 var components = {
     bootstrapfonts: roots.bowercomponents + 'bootstrap/dist/fonts/*.*',
+    appstyles: './ContentDev/**/*.css'
 };
 
-gulp.task('clear', function () {
-    return gulp.src([roots.scripts, roots.fonts, roots.css], { read: false })
-        .pipe(rimraf());
+gulp.task('cmn:clear', function () {
+    return del([roots.scripts + '**/*.js', '!' + roots.scripts + '_references.js', roots.fonts, roots.css]);
 });
 
-gulp.task('dev:build', ['clear'], function () { });
+gulp.task('_dev:build', ['cmn:clear', 'cmn:fonts', 'dev:bundle-lib-js', 'dev:bundle-lib-css-less', 'dev:bundle-app-css'], function () { });
 
-gulp.task('dev:bundle-lib-js', ['clear'], function () {
+gulp.task('dev:bundle-lib-js', ['cmn:clear'], function () {
     return gulp.src('./bower.json')
         .pipe(mainfiles())
-        .pipe(filterJS)
+        .pipe(filters.js)
         .pipe(sourcemaps.init())
             .pipe(concat('lib.js'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(roots.scripts));
+});
+
+gulp.task('dev:bundle-lib-css-less', ['cmn:clear'], function () {
+    return gulp.src('./bower.json')
+        .pipe(mainfiles())
+        .pipe(filters.less)
+        .pipe(sourcemaps.init())
+            .pipe(less())
+            .pipe(concat('lib.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(roots.css));
+});
+
+gulp.task('dev:bundle-app-css', ['cmn:clear'], function () {
+    return gulp.src(components.appstyles)
+        .pipe(sourcemaps.init())
+            .pipe(concat('site.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(roots.css));
+});
+
+gulp.task('cmn:fonts', ['cmn:clear'], function () {
+    return gulp.src([components.bootstrapfonts])
+        .pipe(gulp.dest(roots.fonts))
 });
