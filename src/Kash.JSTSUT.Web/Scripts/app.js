a7329b275109ea7a199ccb0a19e27bb883d43a78
KashPronet <sergio.castillo@pronet-ise.com>
2016-09-15 17:06:27 +0200

    Integración de la api, para luego hacer pruebas sobre ella

--- src/Kash.JSTSUT.Web/Scripts/app.js
+++ src/Kash.JSTSUT.Web/Scripts/app.js
@@ -2,15 +2,16 @@ angular.module('app', []);
 angular.module('app').controller('indexController', function ($http) {
     var vm = this;
 
-    vm.Save = function () {
-            $http.post('/url', {}).then(function (req) {
+    vm.save = function () {
+        $http.post('/Kash.JSTSUT.Web/api/Foos', { Name: vm.Name, Status: vm.Status }).then(function (req) {
                 vm.Name = req.data.Name;
                 vm.Status = req.data.Status;
             });
-        };
-    vm.FillDataForTests = function () {
+    };
+
+    vm.init = function () {
         vm.Name = "Kash";
         vm.Status = "Testing";
     };
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyIsIkluZGV4Q29udHJvbGxlci5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQ0FBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiYW5ndWxhci5tb2R1bGUoJ2FwcCcsIFtdKTsiLCJhbmd1bGFyLm1vZHVsZSgnYXBwJykuY29udHJvbGxlcignaW5kZXhDb250cm9sbGVyJywgZnVuY3Rpb24gKCRodHRwKSB7XHJcbiAgICB2YXIgdm0gPSB0aGlzO1xyXG5cclxuICAgIHZtLlNhdmUgPSBmdW5jdGlvbiAoKSB7XHJcbiAgICAgICAgICAgICRodHRwLnBvc3QoJy91cmwnLCB7fSkudGhlbihmdW5jdGlvbiAocmVxKSB7XHJcbiAgICAgICAgICAgICAgICB2bS5OYW1lID0gcmVxLmRhdGEuTmFtZTtcclxuICAgICAgICAgICAgICAgIHZtLlN0YXR1cyA9IHJlcS5kYXRhLlN0YXR1cztcclxuICAgICAgICAgICAgfSk7XHJcbiAgICAgICAgfTtcclxuICAgIHZtLkZpbGxEYXRhRm9yVGVzdHMgPSBmdW5jdGlvbiAoKSB7XHJcbiAgICAgICAgdm0uTmFtZSA9IFwiS2FzaFwiO1xyXG4gICAgICAgIHZtLlN0YXR1cyA9IFwiVGVzdGluZ1wiO1xyXG4gICAgfTtcclxufSk7Il0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyIsImluZGV4Q29udHJvbGxlci5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQ0FBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyJhbmd1bGFyLm1vZHVsZSgnYXBwJywgW10pOyIsImFuZ3VsYXIubW9kdWxlKCdhcHAnKS5jb250cm9sbGVyKCdpbmRleENvbnRyb2xsZXInLCBmdW5jdGlvbiAoJGh0dHApIHtcclxuICAgIHZhciB2bSA9IHRoaXM7XHJcblxyXG4gICAgdm0uc2F2ZSA9IGZ1bmN0aW9uICgpIHtcclxuICAgICAgICAkaHR0cC5wb3N0KCcvS2FzaC5KU1RTVVQuV2ViL2FwaS9Gb29zJywgeyBOYW1lOiB2bS5OYW1lLCBTdGF0dXM6IHZtLlN0YXR1cyB9KS50aGVuKGZ1bmN0aW9uIChyZXEpIHtcclxuICAgICAgICAgICAgICAgIHZtLk5hbWUgPSByZXEuZGF0YS5OYW1lO1xyXG4gICAgICAgICAgICAgICAgdm0uU3RhdHVzID0gcmVxLmRhdGEuU3RhdHVzO1xyXG4gICAgICAgICAgICB9KTtcclxuICAgIH07XHJcblxyXG4gICAgdm0uaW5pdCA9IGZ1bmN0aW9uICgpIHtcclxuICAgICAgICB2bS5OYW1lID0gXCJLYXNoXCI7XHJcbiAgICAgICAgdm0uU3RhdHVzID0gXCJUZXN0aW5nXCI7XHJcbiAgICB9O1xyXG59KTsiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
--- src/Kash.JSTSUT.Web/ScriptsDev/IndexController.js
+++ src/Kash.JSTSUT.Web/ScriptsDev/IndexController.js
@@ -1,13 +1,14 @@
 ﻿angular.module('app').controller('indexController', function ($http) {
     var vm = this;
 
-    vm.Save = function () {
-            $http.post('/url', {}).then(function (req) {
+    vm.save = function () {
+        $http.post('/Kash.JSTSUT.Web/api/Foos', { Name: vm.Name, Status: vm.Status }).then(function (req) {
                 vm.Name = req.data.Name;
                 vm.Status = req.data.Status;
             });
-        };
-    vm.FillDataForTests = function () {
+    };
+
+    vm.init = function () {
         vm.Name = "Kash";
         vm.Status = "Testing";
     };
